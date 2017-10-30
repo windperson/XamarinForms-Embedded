d1f7a1a890315ce097b403082f93fbb568a667df
Chen Yu Pao <windperson@gmail.com>
2017-10-30 18:10:51 +0800

    An Android project using only Native API

---
+++ TheBaseApp/TheBaseApp.Android/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace TheBaseApp.Droid
+{
+	[Activity (Label = "TheBaseApp.Android", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+		    var ft = FragmentManager.BeginTransaction();
+		    ft.Replace(Resource.Id.base_framelayout, new NativeUiFragment(), "main");
+		    ft.Commit();
+		}
+	}
+}
+
+
---
+++ TheBaseApp/TheBaseApp/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheBaseApp
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
+
